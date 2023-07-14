36bb14dddf9a399a538229759760913d5ac53cb1
sjamilla <j.ssozi@alustudent.com>
2023-07-14 12:53:35 -0700

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         counter = 0
         for i in sublist:
             if counter == (len(sublist) - 1):
-                print("{:d}".format(i), end="")
+                print("{:d}".format(i))
             else:
                 print("{:d}".format(i), end=" ")
             counter += 1
