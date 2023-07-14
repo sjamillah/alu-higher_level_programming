7577827b07c930ae37e7c493a88f7157407bde0a
sjamilla <j.ssozi@alustudent.com>
2023-07-14 13:48:21 -0700

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for sublist in matrix:
-        i = 0
-        for i in sublist:
-            if i == (len(sublist) - 1):
-                print("{:d}".format(i))
-            else:
-                print("{:d}".format(i), end=" ")
-            i+=1
-            print("")
+        if not sublist:
+            print()
+        else:
+            for i in sublist:
+                if i == sublist[-1]:
+                   print("{:d}".format(i))
+                else:
+                   print("{:d}".format(i), end=" ")
