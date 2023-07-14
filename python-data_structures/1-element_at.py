2868fe3de1c7d110ca0e9d5c5ef4a9ae2d76dc65
sjamilla <j.ssozi@alustudent.com>
2023-07-14 11:22:07 -0700

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    length = len(my_list)
-    
-    if idx > length - 1:
+
+    length = len(my_list
+    if idx >= length:
         return (None)
 
     return(my_list[idx])
