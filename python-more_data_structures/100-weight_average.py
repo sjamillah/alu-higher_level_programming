4cf78b9b62c71514f574399a3e445268990ac7af
sjamilla <j.ssozi@alustudent.com>
2023-07-17 07:57:42 -0700

    more data structures

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list:
+    if my_list == []:
         return 0
         numerator = [tuple[0] * tuple[1] for tuple in my_list]
         denominator = [tuple[1] for tuple in my_list]
