7adf727a90c77a12069f18a96459e06c86e27147
sjamilla <j.ssozi@alustudent.com>
2023-07-17 07:23:28 -0700

    exceptions/errors

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    i = 0
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
