1a24d1c0e675c1c113c07fec344eb8bdac7b4ef4
sjamilla <j.ssozi@alustudent.com>
2023-07-24 14:02:55 -0700

    python classes

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -2,8 +2,8 @@
 
 
 class Square:
-    """ A class that defines a square by its size
-    """
+    """ A class that defines a square by its size """
+
     def __str__(self):
         rtn = ""
 
@@ -24,21 +24,18 @@ class Square:
         return rtn
 
     def __init__(self, size=0, position=(0, 0)):
-        """ Method to initialize the square object
-        """
+        """ Method to initialize the square object """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
+        """ Method to returns the size value """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
-        """
+        """ Method to set the size value of the square object """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -47,14 +44,12 @@ class Square:
 
     @property
     def position(self):
-        """ Method that returns the position value
-        """
+        """ Method that returns the position value """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """ Method that sets the position value of a square object
-        """
+        """ Method that sets the position value of a square object """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(value) != 2:
@@ -68,22 +63,15 @@ class Square:
         self.__position = value
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
+        """ Method that returns the square are of the object """
         return (self.__size ** 2)
 
     def my_print(self):
-        """ Method that prints a # square according
-        to the size value
-        """
+        """ Method that prints a # square according to the size value """
         if self.size == 0:
-            print()
-        else:
-            for i in range(self.position[1]):
-                print()
-            for i in range(0, self.size):
-                for k in range(self.position[0]):
-                    print(" ", end='')
-                for j in range(self.size):
-                    print("#", end='')
-                print()
+            print("")
+            return
+
+            print("\n" * self.position[1], end="")
+            for i in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
