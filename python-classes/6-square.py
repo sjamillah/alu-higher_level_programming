0dcf6e4217346dc1ea0932b7942b3d9273e3b703
sjamilla <j.ssozi@alustudent.com>
2023-07-24 13:11:44 -0700

    python classes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,9 +6,33 @@ class Square:
     """ Defines a square """
 
     def __init__(self, size=0, position=(0, 0)):
-         """ Method to initialize the square object """
+         """ Method to initialize the variables """
          self.__size = size
          self.__position = position
+
+    @property
+    def size(self):
+        """ Method to return the value of the size """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Method to set the value of size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        square_area = self.__size ** 2
+        return square_area
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Method to initialize the data """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
