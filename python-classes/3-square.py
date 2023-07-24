64838ec9053873e6a33a1f83e70ef6e1d24a1b9b
sjamilla <j.ssozi@alustudent.com>
2023-07-23 18:46:48 -0700

    python classes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Defines a square """
 
+
 class Square:
     """ Defines a square and creates a private instance attribute """
     def __init__(self, size=0):
@@ -10,7 +11,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = int(size)
 
     def area(self):
         square_area = self.__size ** 2
