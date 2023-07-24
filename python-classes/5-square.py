3e6c34964341c27211f94169d58466f28f9d9435
sjamilla <j.ssozi@alustudent.com>
2023-07-23 19:31:01 -0700

    python classes

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -36,6 +36,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(0, self.__size):
+            for i in range(self.__size):
                 for j in range(self.__size):
-            print("#", end="")
+                    print("#", end="")
+            print()
