56b5e9dcf28274cc84805a030fcd260d00e7ef6e
sjamilla <j.ssozi@alustudent.com>
2023-07-23 20:21:33 -0700

    python classes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,6 +50,10 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            for i in range(self.__size):
-                print("#" * self.size)
- 
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(0, self.__size):
+                for k in range(self.__position[0]):
+                    print(" ", end='')
+                    for j in range(self.___size):
+                print("#" * self.__size)
