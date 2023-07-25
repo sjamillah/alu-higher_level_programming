eb8d8280f7f73bf6dcb65d5b7872f8cb5340cd95
sjamilla <j.ssozi@alustudent.com>
2023-07-25 14:15:49 -0700

    inheritance of classes

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''creates a class Rectangle from parent class BaseGeometry'''
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -9,9 +10,9 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         '''initialize variables'''
-        self.integer_validator('width', width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator('height', height)
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
@@ -21,4 +22,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         '''returns Rectangle width/height'''
-        return f("[Rectangle] {self.__width}/{self.__height}")
+        return f"[Rectangle] {self.__width}/{self.__height}"
