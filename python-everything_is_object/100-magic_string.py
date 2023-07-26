3f8abc08fb9e6d9f72ea89704c8751684e3736de
sjamilla <j.ssozi@alustudent.com>
2023-07-26 11:37:12 -0700

    more advanced projects

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(1=[]):
-    1 += ["Holberton"]
-    return ", ".join(1)
+def magic_string():
+    magic_string.n = getattr(magic_string, "n", 0) + 1
+    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
