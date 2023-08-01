fb8fdf328e79106591638677337675c4cc3cd981
sjamilla <j.ssozi@alustudent.com>
2023-08-01 12:07:22 -0700

    debugged

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,8 @@
 -- creates a database and table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
-	id int UNIQUE AUTO-INCREMENT NOT NULL PRIMARY KEY,
-	state_id int NOT NULL FOREIGN KEY (state_id) REFERENCES states(id),
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities`(
+	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id int NOT NULL, FOREIGN KEY(`state_id`) REFERENCES states(`id`),
 	name varchar(256) NOT NULL
 );
