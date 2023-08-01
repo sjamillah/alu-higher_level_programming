2608089206b88cc6df1d7f912535dd3aef49a2f1
sjamilla <j.ssozi@alustudent.com>
2023-08-01 12:03:13 -0700

    debugged

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates a database and table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS states(
-	id int UNIQUE AUTO-INCREMENT NOT NULL PRIMARY KEY,
+	id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name varchar(256)
 );
