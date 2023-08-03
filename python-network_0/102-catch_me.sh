3871287ce9027029e382ab243c55712de63e2af6
sjamillah <j.ssozi@alustudent.com>
2023-08-03 15:11:49 +0000

    debugged

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -s "0.0.0.0:5000/catch_me_3" | grep -o "You got me!"
+curl -sX POST "0.0.0.0:5000/catch_me_3" -d "You got me!"
