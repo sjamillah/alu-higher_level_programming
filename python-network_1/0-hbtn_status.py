c4007f86d77ebdc0b9f174008ee0c77fa90bc42c
sjamillah <j.ssozi@alustudent.com>
2023-08-03 23:10:47 +0000

    debugged

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 import urllib.request
 
 if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         data = response.read()
