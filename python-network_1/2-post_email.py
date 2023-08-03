640271d50fd557f5a4623242b42240a9eb69f9da
sjamillah <j.ssozi@alustudent.com>
2023-08-03 20:44:41 +0000

    debugged

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """Script that takes in a URL and an email, sends a POST request to the passed URL with email as a parameter and displays the body of the response"""
 
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """"Documented"""
     url = sys.argv[1]
     message = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(message)
+    data = parse.urlencode(message)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
