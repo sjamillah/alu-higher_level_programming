60a1e1aeca7f9d9d7ab9e42e6f04dfb761e5b81e
sjamillah <j.ssozi@alustudent.com>
2023-08-03 20:24:12 +0000

    debugged

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,8 +8,5 @@ if __name__ == '__main__':
     url = 'https://alu-intranet.hbtn.io/status'
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        data = response.read()
+        result = f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode("utf-8")}"
