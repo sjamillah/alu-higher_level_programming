1a7f6488206f44cc52ea627610f715aca2ff85c5
sjamillah <j.ssozi@alustudent.com>
2023-08-03 23:09:13 +0000

    debugged

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Script that fetches a URL"""
 
-
 import urllib.request
 
 if __name__ == '__main__':
@@ -10,3 +9,4 @@ if __name__ == '__main__':
     with urllib.request.urlopen(request) as response:
         data = response.read()
         result = f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode("utf-8")}"
+        print(result)
