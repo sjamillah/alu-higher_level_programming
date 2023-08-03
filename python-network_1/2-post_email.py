47056c903e9734ab7d1911dc754d0300608abb28
sjamillah <j.ssozi@alustudent.com>
2023-08-03 22:43:04 +0000

    debugged

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that takes in a URL and an email, sends a POST request to the passed URL with email as a parameter and displays the body"""
+"""Script that sends a POST request to the passed URL with email as a parameter and displays the body"""
 
 import urllib.request
 import urllib.parse
