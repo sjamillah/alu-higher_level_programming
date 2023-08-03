1cb3133f0a791b8ffe03d5adab7133c487ee747f
sjamillah <j.ssozi@alustudent.com>
2023-08-03 23:00:47 +0000

    debugged

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# script that sends a request to the url and displays the value  of the variable X-Request_Id in the header
+"""script that sends a request to the url and displays the value  of the variable X-Request_Id in the header"""
 
 
 import urllib.request
