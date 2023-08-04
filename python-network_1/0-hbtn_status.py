17cca5194cb6abf90b1850ff08b3e32efcc8da42
sjamillah <j.ssozi@alustudent.com>
2023-08-04 13:45:16 +0000

    fixed errors

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 
 import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(url) as response:
         data = response.read()
-        result = f"Body response:\n\t- type: {type(data)}\n\t- content: {data}\n\t- utf8 content: {data.decode("utf-8")}"
-        print(result)
+        print('Body response:')
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf-8')))
