40aa5e19ef06952a525e5fd922a7af53f32750ad
sjamillah <j.ssozi@alustudent.com>
2023-08-04 13:56:17 +0000

    fixed errors

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''Uses github api to print last ten commits to given repo by given user'''
+"""Uses github api to print last ten commits to given repo by given user"""
+
 from requests import get, auth
 import sys
 
@@ -7,8 +8,8 @@ if __name__ == "__main__":
     try:
        repo = sys.argv[1]
        username = sys.argv[2]
-       url = f"https://api.github.com/repos/{username}/{repo}/commits"
-       response = requests.get(url, headers=headers, params=data)
+       url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+       response = get(url)
        json_data = response.json()
        for entry in (0, 10):
            print(f"{json_data[entry].get('sha')}:, {json_data[entry].get('commit').get('author').get('name')}")
