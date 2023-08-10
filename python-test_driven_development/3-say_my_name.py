81b1ade23becf70b933e60fc7433560562cf7f4d
sjamilla <j.ssozi@alustudent.com>
2023-08-10 16:23:24 -0700

    debugging

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,14 @@
 
 
 def say_my_name(first_name, last_name=""):
-     '''Function to take in names and prints all name
+    """Function to take in names and prints all name
     Args:
         first_name : this is the first arg in a string format
         last_name : must be a string too
 
     Returns:
         My name is <first name> <last name>
-    '''
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
