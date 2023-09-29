eb12d0d2ea6257ed2f3acfcd67fbab882eadfa0a
sjamillah <j.ssozi@alustudent.com>
2023-09-29 14:21:38 -0700

    fixed errors

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,13 +6,14 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
         s += 'X';
       }
-    console.log(s)
+      console.log(s);
     }
   }
 };
